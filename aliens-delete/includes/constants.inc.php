bfcffbc753a3246aa0f4ed472c97d5e4042e7f95
Abbas01 <abbas001@knights.gannon.edu>
2015-04-13 03:27:12 -0400

    new_aliens

---
+++ aliens-delete/includes/constants.inc.php
@@ -0,0 +1,8 @@
+<?php
+// Database Constants
+    define("DB_SERVER", "pendragon.gannon.edu");
+    define("DB_SERVER_LOCAL", "localhost");
+    define("DB_USER", "abbas001");
+    define("DB_PASS", "Id#2669820");
+    define("DB_NAME", "db_abbas001");
+?>
